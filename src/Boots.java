53940909df4d5718a0ec3ca80a1eb456438ece47
dluczun <daniel.luczun.ext@symmetry.pl>
2019-07-07 22:16:31 +0200

    update

--- src/Boots.java
+++ src/Boots.java
@@ -1,2 +1,23 @@
-public class Boots {
+import java.awt.*;
+
+public class Boots extends Product{
+
+    private int size;
+    private boolean isNaturalSkin;
+
+    public Boots(Long id, String productName, Float price, Float weight, Color color, Integer productCount, Integer size, Boolean isNaturalSkin) {
+        super(id, productName, price, weight, color, productCount);
+
+        this.size = size;
+        this.isNaturalSkin = isNaturalSkin;
+    }
+
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isNaturalSkin() {
+        return isNaturalSkin;
+    }
 }
---
+++ src/Cloth.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+
+public class Cloth extends Product{
+
+    private String size;
+    private String material;
+
+    public Cloth(Long id, String productName, Float price, Float weight, Color color, Integer productCount, String size, String material) {
+        super(id, productName, price, weight, color, productCount);
+
+        this.size = size;
+        this.material = material;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getMaterial() {
+        return material;ś
+    }
+}
