58715275ddb501ae1fa8e41ebd9e3ad2ade03714
dluczun <daniel.luczun.ext@symmetry.pl>
2019-07-07 22:03:05 +0200

    init commit

--- src/User.java
+++ src/User.java
@@ -3,13 +3,11 @@ public class User {
     private Long id;
     private String login;
     private String password;
-    private String email;
 
     public User(Long id, String login, String password, String email){
         this.id = id;
         this.login = login;
         this.password = password;
-        this.email = email;
     }
 
     public Long getId(){
@@ -24,17 +22,10 @@ public class User {
         return password;
     }
 
-    public String getEmail(){
-        return email;
-    }
-
     public void setLogin(String login) {
         this.login = login;
     }
     public void setPassword(String password) {
         this.password = password;
-    }
-    public void setEmail(String email) {
-        this.email = email;
     }
 }
